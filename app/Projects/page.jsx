8ade9b05f09107546ea8a175a7b55044aed6b284
Ali-Elchab <alielchab01@gmail.com>
2024-04-16 16:37:20 +0300

    Added projects

--- app/Projects/page.jsx
+++ app/Projects/page.jsx
@@ -14,7 +14,7 @@ const projectsData = [
 
   {
     id: 2,
-    imgUrl: "/images/projects/netflix-project.png",
+    imgUrl: "/images/projects/Group1.png",
     title: "Netflix Replica",
     description:
       "This project is a collaborative effort to clone the Netflix website, focusing on a smooth user experience and responsive design. Built using React, this application demonstrates my ability to replicate complex UIs and implement functionality that mimics the real Netflix platform.",
@@ -24,12 +24,28 @@ const projectsData = [
 
   {
     id: 3,
-    imgUrl: "/images/projects/survey.jpg",
+    imgUrl: "/images/projects/survey.png",
     title: "Survey Builder",
     description:
       "Survey builder website that empowers users to create customized surveys effortlessly. Leveraging the power of React for frontend development and Laravel for backend support. Users can craft surveys tailored to their needs, whether it's for market research, customer feedback, or academic purposes. ",
     gitUrl: "https://github.com/Ali-Elchab/survey-builder",
     previewUrl: "",
+  },
+  {
+    id: 4,
+    imgUrl: "/images/projects/BankingSystem.png",
+    title: "Banking System",
+    description:
+      "Simple banking system developed using .NET WinForms and C# programming language with Entity Framework for database operations. The backend database management system used for this project is SQL Server.",
+    gitUrl: "https://github.com/Ali-Elchab/BankingSystem",
+  },
+  {
+    id: 9,
+    imgUrl: "/images/projects/simon-says.jpg",
+    title: "Simon Says Game",
+    description:
+      "Simon Says game developed using raw HTML, CSS, and JavaScript. Simon Says is a memory skill game that challenges players to follow an increasingly complex series of lights and sounds.",
+    gitUrl: "https://github.com/Ali-Elchab/SimonSays",
   },
 ];
 
@@ -37,7 +53,7 @@ const ProjectSection = () => {
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-4xl font-bold text-white mb-16">My Projects</h2>
-      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+      <div className="grid md:grid-cols-3 gap-8 md:gap-8">
         {projectsData.map((project) => (
           <ProjectCard
             key={project.id}
@@ -45,7 +61,6 @@ const ProjectSection = () => {
             description={project.description}
             imgUrl={project.imgUrl}
             gitUrl={project.gitUrl}
-            previewUrl={project.previewUrl}
           />
         ))}
       </div>
--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
-const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
+const ProjectCard = ({ imgUrl, title, description, gitUrl }) => {
   return (
     <div>
       <div className="h-52 md:h-72 rounded-t-xl relative group" style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}>
